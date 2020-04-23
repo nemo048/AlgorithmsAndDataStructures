25b49a6f87226f96a3c496245e34c385d612360b
Василий Саурин <saurin048@ya.ru>
2020-04-23 15:59:16 +0200

    Initial commit

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int _value) { value = _value; }
+    }
+
+    public class LinkedList
+    {
+        public Node head;
+        public Node tail;
+        private int count;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public void AddInTail(Node _item)
+        {
+            if (head == null)
+            {
+                head = _item;
+            }
+            else
+            {
+                tail.next = _item;
+            }
+            tail = _item;
+
+            count++;
+        }
+
+        public Node Find(int _value)
+        {
+            Node node = head;
+            while (node != null)
+            {
+                if (node.value == _value)
+                {
+                    return node;
+                }
+                node = node.next;
+            }
+            return null;
+        }
+
+        public List<Node> FindAll(int _value)
+        {
+            List<Node> nodes = new List<Node>();
+            // здесь будет ваш код поиска всех узлов по заданному значению
+            Node node = head;
+            while (node != null)
+            {
+                if (node.value == _value)
+                {
+                    nodes.Add(node);
+                }
+                node = node.next;
+            }
+            return nodes;
+        }
+
+        public bool Remove(int _value)
+        {
+            // здесь будет ваш код удаления одного узла по заданному значению
+            Node node = head;
+            Node parent = null;
+
+            while (node != null)
+            {
+                if (node.value == _value)
+                {
+                    if (parent == null)
+                    {
+                        head = node.next;
+                        if (head == null)
+                        {
+                            tail = null;
+                        }
+                    }
+                    else
+                    {
+                        parent.next = node.next;
+                    }
+
+                    count--;
+                    return true; // если узел был удалён
+                }
+
+                parent = node;
+                node = node.next;
+            }
+            return false; 
+        }
+
+        public void RemoveAll(int _value)
+        {
+            // здесь будет ваш код удаления всех узлов по заданному значению
+            Node node = head;
+            Node parent = null;
+            
+            while (node != null)
+            {
+                if (node.value == _value)
+                {
+                    if (parent == null)
+                    {
+                        head = node.next;
+                        if (head == null)
+                        {
+                            tail = null;
+                        }
+
+                        parent = null;
+                    }
+                    else
+                    {
+                        parent.next = node.next;
+                    }
+
+                    count--;
+                    node = node.next;
+                    continue;
+                }
+
+                parent = node;
+                node = node.next;
+            }
+        }
+
+        public void Clear()
+        {
+            // здесь будет ваш код очистки всего списка
+            while (head != null)
+            {
+                head = head.next;
+            }
+
+            tail = null;
+            count = 0;
+        }
+
+        public int Count()
+        {
+            return count; // здесь будет ваш код подсчёта количества элементов в списке
+        }
+
+        public void InsertAfter(Node _nodeAfter, Node _nodeToInsert)
+        {
+            count++;
+            // если _nodeAfter = null , 
+            // добавьте новый элемент первым в списке 
+            if (_nodeAfter == null)
+            {
+                _nodeToInsert.next = head;
+                head = _nodeToInsert;
+                return;
+            }
+
+            Node node = head;
+            Node parent = null;
+            
+            while (node != null)
+            {
+                if (node == _nodeAfter)
+                {
+                    _nodeToInsert.next = node.next;
+                    if (node.next == null)
+                    {
+                        tail = node.next;
+                    }
+                    node.next = _nodeToInsert;
+                    return;
+                }
+
+                node = node.next;
+            }
+        }
+    }
+}
---
+++ LinkedListTest/Tests.cs
@@ -0,0 +1,143 @@
+﻿using System.Linq;
+using AlgorithmsDataStructures;
+using Xunit;
+
+namespace LinkedListTest
+{
+    public class Tests
+    {
+        #region Universal methods
+
+        private Node[] BuildNodes(params int[] values)
+        {
+            Node[] nodes = new Node[values.Length];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                nodes[i] = new Node(values[i]);
+            }
+
+            return nodes;
+        }
+
+        private LinkedList GetLinkedList(params Node[] nodes)
+        {
+            LinkedList list = new LinkedList();
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                list.AddInTail(nodes[i]);
+            }
+
+            if (nodes.Length == 0)
+            {
+                Assert.Null(list.head);
+                Assert.Null(list.tail);
+            }
+            else
+            {
+                Assert.NotNull(list.head);
+                Assert.NotNull(list.tail);
+            }
+
+            return list;
+        }
+        
+        private void RemoveTest(int valueToRemove, params Node[] nodes)
+        {
+            LinkedList list = GetLinkedList(nodes);
+
+            if (nodes.Count(node => node.value == valueToRemove) >= 1)
+            {
+                Assert.True(list.Remove(valueToRemove));
+                Assert.Equal(nodes.Length - 1, list.Count());
+                if (nodes.Length - 1 == 0)
+                {
+                    Assert.Null(list.head);
+                    Assert.Null(list.tail);
+                }
+                else
+                {
+                    Assert.NotNull(list.head);
+                    Assert.NotNull(list.tail);
+                }
+            }
+            else
+            {
+                Assert.False(list.Remove(valueToRemove));
+                Assert.Equal(nodes.Length, list.Count());
+                if (nodes.Length == 0)
+                {
+                    Assert.Null(list.head);
+                    Assert.Null(list.tail);
+                }
+                else
+                {
+                    Assert.NotNull(list.head);
+                    Assert.NotNull(list.tail);
+                }
+            }
+        }
+        
+        private void RemoveAllTest(int valueToRemove, params Node[] nodes)
+        {
+            LinkedList list = GetLinkedList(nodes);
+
+            int numberOfNodesToRemove = nodes.Count(node => node.value == valueToRemove);
+            list.RemoveAll(valueToRemove);
+
+            if (numberOfNodesToRemove >= 1)
+            {
+                int expectedCount = nodes.Length - numberOfNodesToRemove;
+                Assert.Equal(expectedCount, list.Count());
+                if (expectedCount == 0)
+                {
+                    Assert.Null(list.head);
+                    Assert.Null(list.tail);
+                }
+                else
+                {
+                    Assert.NotNull(list.head);
+                    Assert.NotNull(list.tail);
+                }
+            }
+            else
+            {
+                Assert.Equal(nodes.Length, list.Count());
+                if (nodes.Length == 0)
+                {
+                    Assert.Null(list.head);
+                    Assert.Null(list.tail);
+                }
+                else
+                {
+                    Assert.NotNull(list.head);
+                    Assert.NotNull(list.tail);
+                }
+            }
+        }
+
+        #endregion
+
+        [Fact]
+        public void RemoveTest()
+        {
+            RemoveTest(0);
+            RemoveTest(1, BuildNodes(1));
+            RemoveTest(1, BuildNodes(2));
+            RemoveTest(0, BuildNodes(0, 1, 2));
+            RemoveTest(0, BuildNodes(0, 0, 0));
+            RemoveTest(0, BuildNodes(0, 1, 0));
+        }
+
+        [Fact]
+        public void RemoveAllTest()
+        {
+            RemoveAllTest(0);
+            RemoveAllTest(1, BuildNodes(1));
+            RemoveAllTest(1, BuildNodes(2));
+            RemoveAllTest(0, BuildNodes(0, 1, 2));
+            RemoveAllTest(0, BuildNodes(0, 0, 0));
+            RemoveAllTest(0, BuildNodes(0, 1, 0));
+        }
+    }
+}
