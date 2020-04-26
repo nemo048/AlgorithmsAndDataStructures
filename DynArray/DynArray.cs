5b989f1d9f4474bc80f4dd9a8784343a3703ce1e
Василий Саурин <saurin048@ya.ru>
2020-04-26 19:32:33 +0200

    Added DynArray implementations and tests

---
+++ DynArray/DynArray.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class DynArray<T>
+    {
+        public T [] array;
+        public int count;
+        public int capacity;
+        
+        private const int MIN_CAPACITY = 16;
+        private float _fillPercent = 0.5f;
+        
+        public DynArray()
+        {
+            count = 0;
+            MakeArray(MIN_CAPACITY);
+        }
+
+        public void MakeArray(int new_capacity)
+        {
+            // ваш код
+            capacity = new_capacity < MIN_CAPACITY ? MIN_CAPACITY : new_capacity;
+            T[] newArray = new T[capacity];
+            if (array != null)
+            {
+                Array.Copy(array, newArray, count);
+            }
+            array = newArray;
+        }
+
+        public T GetItem(int index)
+        {
+            if (index > count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+            // ваш код
+            return array[index];
+        }
+
+        public void Append(T itm)
+        {
+            // ваш код
+            if (count >= capacity)
+            {
+                MakeArray(capacity * 2);
+            }
+            array[count++] = itm;
+        }
+
+        public void Insert(T itm, int index)
+        {
+            // ваш код
+            if (index > count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            if (count + 1 >= capacity)
+            {
+                MakeArray(capacity * 2);
+            }
+
+            T old = array[index];
+            array[index] = itm;
+            
+            for (int i = index + 1; i < count + 1; i++)
+            {
+                T temp = array[i];
+                array[i] = old;
+                old = temp;
+            }
+
+            count++;
+        }
+
+        public void Remove(int index)
+        {
+            // ваш код
+            if (index > count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            count--;
+            T old = array[count];
+            array[count] = default(T);
+
+            for (int i = count - 1; i >= index; i--)
+            {
+                T temp = array[i];
+                array[i] = old;
+                old = temp;
+            }
+
+            if ((float) (count - 1) / capacity < _fillPercent)
+            {
+                MakeArray((int)(capacity / 1.5));
+            }
+        }
+
+    }
+}
---
+++ DynArrayTests/Tests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using AlgorithmsDataStructures;
+using Xunit;
+
+namespace DynArrayTests
+{
+    public class Tests
+    {
+        private void MakeAppend(DynArray<int> array, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                array.Append(42);
+            }
+        }
+
+        [Fact]
+        public void AppendWithoutExtendBufferTest()
+        {
+            //вставка элемента, когда в итоге размер буфера не превышен (проверьте также размер буфера);
+            DynArray<int> array = new DynArray<int>();
+
+            array.Append(42);
+
+            Assert.Equal(1, array.count);
+            Assert.Equal(16, array.array.Length);
+            Assert.Equal(42, array.GetItem(0));
+            Assert.Equal(16, array.capacity);
+        }
+
+        [Fact]
+        public void AppendWithExtendBufferTest()
+        {
+            //вставка элемента, когда в результате превышен размер буфера (проверьте также корректное изменение размера буфера);
+            DynArray<int> array = new DynArray<int>();
+            
+            MakeAppend(array, 17);
+            
+            Assert.Equal(17, array.count);
+            Assert.Equal(32, array.array.Length);
+            for (int i = 0; i < array.count; i++)
+            {
+                Assert.Equal(42, array.GetItem(i));
+            }
+            Assert.Equal(32, array.capacity);
+        }
+
+        [Fact]
+        public void InsertToIncorrectPositionTest()
+        {
+            //попытка вставки элемента в недопустимую позицию;
+            DynArray<int> array = new DynArray<int>();
+            
+            MakeAppend(array, 6);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => array.Insert(33, 8));
+        }
+
+        [Fact]
+        public void RemoveWithoutDecreaseBufferSizeTest()
+        {
+            //удаление элемента, когда в результате размер буфера остаётся прежним (проверьте также размер буфера);
+            DynArray<int> array = new DynArray<int>();
+            
+            MakeAppend(array, 4);
+            
+            array.Append(33);
+            
+            MakeAppend(array, 4);
+            
+            array.Remove(4);
+            
+            Assert.Equal(8, array.count);
+            Assert.Equal(16, array.array.Length);
+            Assert.Equal(42, array.GetItem(4));
+            Assert.Throws<ArgumentOutOfRangeException>(() => array.GetItem(9));
+            Assert.Equal(16, array.capacity);
+        }
+
+        [Fact]
+        public void RemoveWithDecreaseBufferSizeTest()
+        {
+            //удаление элемента, когда в результате понижается размер буфера (проверьте также корректное изменение размера буфера);
+            DynArray<int> array = new DynArray<int>();
+            
+            MakeAppend(array, 12);
+            
+            array.Append(33);
+            
+            MakeAppend(array, 4);
+            
+            array.Remove(12);
+            
+            Assert.Equal(16, array.count);
+            Assert.Equal(21, array.array.Length);
+            Assert.Equal(42, array.GetItem(12));
+            Assert.Throws<ArgumentOutOfRangeException>(() => array.GetItem(22));
+            Assert.Equal(21, array.capacity);
+        }
+
+        [Fact]
+        public void RemoveFromIncorrectPositionTest()
+        {
+            //попытка удаления элемента в недопустимой позиции.
+            DynArray<int> array = new DynArray<int>();
+            
+            MakeAppend(array, 6);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => array.Remove(7));
+        }
+    }
+}
