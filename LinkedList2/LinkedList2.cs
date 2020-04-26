7b9232aa68b0cba9674a99f0f58ac89226cabc5c
Василий Саурин <saurin048@ya.ru>
2020-04-26 14:03:22 +0200

    Added LinkedList2

---
+++ LinkedList2/LinkedList2.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class Node
+    {
+        public int value;
+        public Node next, prev;
+
+        public Node(int _value) { 
+            value = _value; 
+            next = null;
+            prev = null;
+        }
+    }
+
+    public class LinkedList2
+    {
+        public Node head;
+        public Node tail;
+        private int count;
+
+        public LinkedList2()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public void AddInTail(Node _item)
+        {
+            if (head == null) {
+                head = _item;
+                head.next = null;
+                head.prev = null;
+            } else {
+                tail.next = _item;
+                _item.prev = tail;
+            }
+            tail = _item;
+            
+            count++;
+        }
+
+        public Node Find(int _value)
+        {
+            // здесь будет ваш код поиска
+            Node node = head;
+            while (node != null)
+            {
+                if (node.value == _value)
+                {
+                    return node;
+                }
+                node = node.next;
+            }
+            return null;
+        }
+
+        public List<Node> FindAll(int _value)
+        {
+            List<Node> nodes = new List<Node>();
+            // здесь будет ваш код поиска всех узлов по заданному значению
+            Node node = head;
+            while (node != null)
+            {
+                if (node.value == _value)
+                {
+                    nodes.Add(node);
+                }
+                node = node.next;
+            }
+            return nodes;
+        }
+
+        private bool Remove(Node node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (node.prev == null)
+            {
+                if (node.next == null)
+                {
+                    head = null;
+                    tail = null;
+                }
+                else
+                {
+                    head = node.next;
+                    head.prev = null;
+                    node.next = null;
+                }
+            }
+            else if (node.next == null)
+            {
+                tail = node.prev;
+                tail.next = null;
+                node.prev = null;
+            }
+            else
+            {
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+                node.next = null;
+                node.prev = null;
+                node = null;
+            }
+
+            count--;
+            
+            return true; 
+        }
+
+        public bool Remove(int _value)
+        {
+            // здесь будет ваш код удаления одного узла по заданному значению
+            Node node = Find(_value);
+
+            return Remove(node);
+        }
+
+        public void RemoveAll(int _value)
+        {
+            // здесь будет ваш код удаления всех узлов по заданному значению
+            foreach (Node node in FindAll(_value))
+            {
+                Remove(node);
+            }
+        }
+
+        public void Clear()
+        {
+            // здесь будет ваш код очистки всего списка
+            Node node = head;
+
+            while (node != null)
+            {
+                if (node.prev != null)
+                {
+                    node.prev.next = null;
+                    node.prev = null;
+                }
+                node = node.next;
+            }
+
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public int Count()
+        {
+            return count; // здесь будет ваш код подсчёта количества элементов в списке
+        }
+
+        public void AddInHead(Node node)
+        {
+            count++;
+
+            if (head == null)
+            {
+                AddInTail(node);
+                return;
+            }
+
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+
+        public void InsertAfter(Node _nodeAfter, Node _nodeToInsert)
+        {
+            // здесь будет ваш код вставки узла после заданного узла
+            count++;
+            // если _nodeAfter = null , 
+            // добавьте новый элемент первым в списке 
+            if (_nodeAfter == null)
+            {
+                if (count == 1)
+                {
+                    head = _nodeToInsert;
+                    head.prev = null;
+                    head.next = null;
+                    tail = _nodeToInsert;
+                    tail.prev = null;
+                    tail.next = null;
+                }
+                else
+                {
+                    _nodeToInsert.next = head;
+                    _nodeToInsert.prev = null;
+                    
+                    head.prev = _nodeToInsert;
+                    head = _nodeToInsert;
+                }
+
+                return;
+            }
+
+            Node node = head;
+            
+            while (node != null)
+            {
+                if (node == _nodeAfter)
+                {
+                    if (_nodeAfter == tail)
+                    {
+                        _nodeAfter.next = _nodeToInsert;
+                        _nodeToInsert.prev = _nodeAfter;
+                        tail = _nodeToInsert;
+                    }
+                    else
+                    {
+                        _nodeToInsert.next = _nodeAfter.next;
+                        _nodeToInsert.prev = _nodeAfter;
+                        _nodeAfter.next.prev = _nodeToInsert;
+                        _nodeAfter.next = _nodeToInsert;
+                    }
+                    return;
+                }
+
+                node = node.next;
+            }
+        }
+
+    }
+}
---
+++ LinkedList2Tests/Tests.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmsDataStructures;
+using Xunit;
+
+namespace LinkedList2Tests
+{
+    public class NodeValueEqualityComparer : IEqualityComparer<Node>
+    {
+        /// <inheritdoc />
+        public bool Equals(Node first, Node second)
+        {
+            if (first == null && second == null)
+            {
+                return true;
+            }
+            
+            if (first == null || second == null)
+            {
+                return false;
+            }
+            
+            return first.value == second.value;
+        }
+
+        /// <inheritdoc />
+        public int GetHashCode(Node obj)
+        {
+            return obj.value;
+        }
+    }
+    
+    public class Tests
+    {
+        #region Universal methods
+
+        private Node[] BuildNodes(params int[] values)
+        {
+            Node[] nodes = new Node[values.Length];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                nodes[i] = new Node(values[i]);
+            }
+
+            return nodes;
+        }
+        
+        private LinkedList2 GetLinkedList(params Node[] nodes)
+        {
+            LinkedList2 list = new LinkedList2();
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                list.AddInTail(nodes[i]);
+            }
+
+            switch (nodes.Length)
+            {
+                case 0:
+                    Assert.Null(list.head);
+                    Assert.Null(list.tail);
+                    break;
+                case 1:
+                    Assert.NotNull(list.head);
+                    Assert.Null(list.head.prev);
+                    Assert.Null(list.head.next);
+                    Assert.NotNull(list.tail);
+                    Assert.Null(list.tail.prev);
+                    Assert.Null(list.tail.next);
+                    break;
+                default:
+                    Assert.NotNull(list.head);
+                    Assert.Null(list.head.prev);
+                    Assert.NotNull(list.head.next);
+                    Assert.NotNull(list.tail);
+                    Assert.NotNull(list.tail.prev);
+                    Assert.Null(list.tail.next);
+                    break;
+            }
+
+            return list;
+        }
+
+        private void CheckPrevAndNextCorrectness(LinkedList2 list, int length)
+        {
+            switch (length)
+            {
+                case 0:
+                    Assert.Null(list.head);
+                    Assert.Null(list.tail);
+                    return;
+                case 1:
+                    Assert.Same(list.head, list.tail);
+                    Assert.Null(list.head.prev);
+                    Assert.Null(list.head.next);
+                    Assert.Null(list.tail.prev);
+                    Assert.Null(list.tail.next);
+                    return;
+                default:
+                    Assert.NotNull(list.head);
+                    Assert.Null(list.head.prev);
+                    Assert.NotNull(list.head.next);
+                    Assert.NotNull(list.tail);
+                    Assert.NotNull(list.tail.prev);
+                    Assert.Null(list.tail.next);
+                    
+                    Node node = list.head;
+                    while (node != null)
+                    {
+                        if (node.next != null)
+                        {
+                            Assert.Same(node, node.next.prev);
+                        }
+                        node = node.next;
+                    }
+                    break;
+            }
+        }
+        
+        private void RemoveTest(int valueToRemove, params Node[] nodes)
+        {
+            LinkedList2 list = GetLinkedList(nodes);
+            int nodesToRemove = nodes.Count(node => node.value == valueToRemove) >= 1 ? 1 : 0;
+            Node expectedHead = nodes.FirstOrDefault(node => node.value != valueToRemove) ??
+                                nodes.Where(node => node.value == valueToRemove).Skip(1).FirstOrDefault();
+            Node expectedTail = expectedHead == null ? 
+                null : 
+                nodes.LastOrDefault();
+
+            bool removeSuccess = list.Remove(valueToRemove);
+            
+            Assert.True(nodesToRemove == 0 && !removeSuccess || 
+                        nodesToRemove == 1 && removeSuccess);
+            Assert.Equal(nodes.Length - nodesToRemove, list.Count());
+            Assert.Same(expectedHead, list.head);
+            Assert.Same(expectedTail, list.tail);
+
+            CheckPrevAndNextCorrectness(list, nodes.Length - nodesToRemove);
+        }
+
+        private void RemoveAllTest(int valueToRemove, params Node[] nodes)
+        {
+            LinkedList2 list = GetLinkedList(nodes);
+
+            int nodesToRemove = nodes.Count(node => node.value == valueToRemove);
+            Node expectedHead = nodes.FirstOrDefault(node => node.value != valueToRemove);
+            Node expectedTail = nodes.LastOrDefault(node => node.value != valueToRemove);
+            
+            list.RemoveAll(valueToRemove);
+
+            Assert.Equal(nodes.Length - nodesToRemove, list.Count());
+            
+            Assert.Same(expectedHead, list.head);
+            Assert.Same(expectedTail, list.tail);
+
+            CheckPrevAndNextCorrectness(list, nodes.Length - nodesToRemove);
+        }
+
+        private void FindTest(int valueToFind, params Node[] nodes)
+        {
+            LinkedList2 list = GetLinkedList(nodes);
+
+            Node expectedNode = nodes.FirstOrDefault(n => n.value == valueToFind);
+            
+            Assert.Same(expectedNode, list.Find(valueToFind));
+        }
+
+        private void FindAllTest(int valueToFind, params Node[] nodes)
+        {
+            LinkedList2 list = GetLinkedList(nodes);
+
+            IEnumerable<Node> expectedNodes = nodes.Where(node => node.value == valueToFind);
+            
+            Assert.True(expectedNodes.SequenceEqual(
+                list.FindAll(valueToFind),
+                new NodeValueEqualityComparer()));
+        }
+
+        private void InsertAfterTest(Node nodeToInsert, int nodeAfterNumber, params Node[] nodes)
+        {
+            Node nodeAfter = nodeAfterNumber < nodes.Length ? nodes[nodeAfterNumber] : null;
+
+            LinkedList2 list = GetLinkedList(nodes);
+
+            Node expectedHead = nodeAfterNumber == 0 ? 
+                nodeAfter ?? nodeToInsert : 
+                list.head;
+            Node expectedTail = nodeAfterNumber == nodes.Length - 1 || nodes.Length == 0 ? nodeToInsert : list.tail;
+            
+            list.InsertAfter(nodeAfter, nodeToInsert);
+
+            if (nodeAfter != null)
+            {
+                Assert.Same(nodeAfter.next, nodeToInsert);
+            }
+            Assert.Same(expectedHead, list.head);
+            Assert.Same(expectedTail, list.tail);
+            
+            CheckPrevAndNextCorrectness(list, nodes.Length + 1);
+        }
+
+        private void AddInHeadTest(Node nodeToInsert, params Node[] nodes)
+        {
+            LinkedList2 list = GetLinkedList(nodes);
+
+            Node expectedHead = nodeToInsert;
+            Node expectedTail = nodes.LastOrDefault() ?? nodeToInsert;
+            
+            list.AddInHead(nodeToInsert);
+            
+            Assert.Same(expectedHead, list.head);
+            Assert.Same(expectedTail, list.tail);
+            
+            CheckPrevAndNextCorrectness(list, nodes.Length + 1);
+        }
+        
+        private void ClearTest(params Node[] nodes)
+        {
+            LinkedList2 list = GetLinkedList(nodes);
+            
+            list.Clear();
+            
+            Assert.Null(list.head);
+            Assert.Null(list.tail);
+        }
+
+        private void CountTest(int expectedLength, params Node[] nodes)
+        {
+            LinkedList2 list = GetLinkedList(nodes);
+            
+            Assert.Equal(expectedLength, list.Count());
+        }
+
+        #endregion
+
+        [Fact]
+        public void FindTest()
+        {
+            FindTest(0);
+            FindTest(1, BuildNodes(1));
+            FindTest(1, BuildNodes(2));
+            FindTest(0, BuildNodes(0, 1, 2));
+            FindTest(0, BuildNodes(0, 0, 0));
+            FindTest(0, BuildNodes(0, 1, 0));
+        }
+
+        [Fact]
+        public void FindAllTest()
+        {
+            FindAllTest(0);
+            FindAllTest(1, BuildNodes(1));
+            FindAllTest(1, BuildNodes(2));
+            FindAllTest(0, BuildNodes(0, 1, 2));
+            FindAllTest(0, BuildNodes(0, 0, 0));
+            FindAllTest(0, BuildNodes(0, 1, 0));
+        }
+        
+        [Fact]
+        public void RemoveTest()
+        {
+            RemoveTest(0);
+            RemoveTest(1, BuildNodes(1));
+            RemoveTest(1, BuildNodes(2));
+            RemoveTest(0, BuildNodes(0, 1, 2));
+            RemoveTest(0, BuildNodes(0, 0, 0));
+            RemoveTest(0, BuildNodes(0, 1, 0));
+        }
+
+        [Fact]
+        public void RemoveAllTest()
+        {
+            RemoveAllTest(0);
+            RemoveAllTest(1, BuildNodes(1));
+            RemoveAllTest(1, BuildNodes(2));
+            RemoveAllTest(0, BuildNodes(0, 1, 2));
+            RemoveAllTest(0, BuildNodes(0, 0, 0));
+            RemoveAllTest(0, BuildNodes(0, 1, 0));
+        }
+
+        [Fact]
+        public void InsertAfterTest()
+        {
+            InsertAfterTest(new Node(0), 0);
+            InsertAfterTest(new Node(0), 0, BuildNodes(1));
+            InsertAfterTest(new Node(0), 2, BuildNodes(1, 2, 3));
+            InsertAfterTest(new Node(0), 1, BuildNodes(1, 2, 3));
+        }
+
+        [Fact]
+        public void AddInHead()
+        {
+            AddInHeadTest(new Node(0));
+            AddInHeadTest(new Node(0), BuildNodes(1));
+            AddInHeadTest(new Node(0), BuildNodes(1, 2, 3));
+            AddInHeadTest(new Node(0), BuildNodes(1, 2, 3));
+        }
+
+        [Fact]
+        public void ClearTest()
+        {
+            ClearTest(BuildNodes());
+            ClearTest(BuildNodes(0, 1, 2));
+        }
+
+        [Fact]
+        public void CountTest()
+        {
+            CountTest(0, BuildNodes());
+            CountTest(1, BuildNodes(1));
+            CountTest(3, BuildNodes(0, 0, 0));
+        }
+    }
+}
