14eb277f4b26824f454f7a7fefef73bb0590e9ff
Василий Саурин <saurin048@ya.ru>
2020-04-29 16:52:11 +0200

    Added Stack and its tests

---
+++ Stack/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class Stack<T>
+    {
+        private readonly DynArray<T> _array;
+        
+        public Stack()
+        {
+            // инициализация внутреннего хранилища стека
+            _array = new DynArray<T>();
+        } 
+
+        public int Size() 
+        {
+            // размер текущего стека		  
+            return _array.count;
+        }
+
+        public T Pop()
+        {
+            // ваш код
+            if (_array.count == 0)
+            {
+                return default(T); // null, если стек пустой
+            }
+
+            T item = _array.GetItem(_array.count - 1);
+            _array.Remove(_array.count - 1);
+            return item;
+        }
+	  
+        public void Push(T val)
+        {
+            // ваш код
+            _array.Append(val);
+        }
+
+        public T Peek()
+        {
+            // ваш код
+            if (_array.count == 0)
+            {
+                return default(T); // null, если стек пустой
+            }
+
+            return _array.GetItem(_array.count - 1);
+        }
+    }
+
+}
---
+++ StackTests/Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using AlgorithmsDataStructures;
+using Xunit;
+
+namespace StackTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void PushTest()
+        {
+            Stack<int?> stack = new Stack<int?>();
+            
+            stack.Push(1);
+            
+            Assert.Equal(1, stack.Size());
+            Assert.Equal(1, stack.Peek());
+        }
+
+        [Fact]
+        public void SizeTest()
+        {
+            Stack<int> stack = new Stack<int>();
+            
+            stack.Push(1);
+            Assert.Equal(1, stack.Size());
+            stack.Push(2);
+            Assert.Equal(2, stack.Size());
+            stack.Push(3);
+            Assert.Equal(3, stack.Size());
+        }
+
+        [Fact]
+        public void PopTest()
+        {
+            Stack<int?> stack = new Stack<int?>();
+            
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            
+            Assert.Equal(3, stack.Pop());
+            Assert.Equal(2, stack.Pop());
+            Assert.Equal(1, stack.Pop());
+            Assert.Null(stack.Pop());
+        }
+
+        [Fact]
+        public void PeekTest()
+        {
+            Stack<int?> stack = new Stack<int?>();
+            
+            Assert.Null(stack.Peek());
+            
+            stack.Push(1);
+            
+            Assert.Equal(1, stack.Peek());
+            Assert.Equal(1, stack.Peek());
+        }
+    }
+}
