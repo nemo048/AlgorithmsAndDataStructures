d41342b055852199c93dbaa911c6365bcf15db56
Василий Саурин <saurin048@ya.ru>
2020-05-08 18:00:13 +0200

    Added HashTable

---
+++ HashTable/HashTable.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class HashTable
+    {
+        public int size;
+        public int step;
+        public string [] slots; 
+
+        public HashTable(int sz, int stp)
+        {
+            size = sz;
+            step = stp;
+            slots = new string[size];
+            for(int i=0; i<size; i++) slots[i] = null;
+        }
+
+        public int HashFun(string value)
+        {    
+            // всегда возвращает корректный индекс слота
+            return Math.Abs(value.GetHashCode()) % size;
+        }
+
+        public int SeekSlot(string value)
+        {
+            // находит индекс пустого слота для значения, или -1
+
+            int index = HashFun(value);
+
+            int firstIndex = index;
+            while (slots[index] != null)
+            {
+                index = (index + step) % size;
+
+                if (index == firstIndex)
+                {
+                    return -1;
+                }
+            }
+
+            return index;
+        }
+
+        public int Put(string value)
+        {
+            // записываем значение по хэш-функции
+            int index = SeekSlot(value);
+
+            if (index == -1)
+            {
+                return -1;
+            }
+            
+            slots[index] = value;
+            return index;
+        }
+
+        public int Find(string value)
+        {
+            // находит индекс слота со значением, или -1
+            int index = HashFun(value);
+
+            if (slots[index] == value)
+            {
+                return index;
+            }
+
+            int firstIndex = index;
+            while (slots[index] != value)
+            {
+                index = (index + step) % size;
+
+                if (index == firstIndex)
+                {
+                    return -1;
+                }
+            }
+
+            return index;
+        }
+    }
+ 
+}
---
+++ HashTableTests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AlgorithmsDataStructures;
+using Xunit;
+
+namespace HashTableTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void HashFunTest()
+        {
+            HashTable hashTable = new HashTable(17, 3);
+
+            int index = hashTable.HashFun("hello world");
+        }
+    }
+}
